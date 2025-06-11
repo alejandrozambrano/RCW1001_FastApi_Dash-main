c4801d59d2474e2eeb48559f17306d90f74d216f
Alejandro Zambrano <alejandro_zambrano_@hotmail.com>
2025-06-10 21:36:26 -0400

    port 8000
    error of using port 8000 in other project

--- fastapi_app/startup.sh
+++ fastapi_app/startup.sh
@@ -1,4 +1,5 @@
 
 
 # Run The app : FastApi + Dash using Uvicorn
-Uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8000 --workes 1
+uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8000 --workers 1
+
